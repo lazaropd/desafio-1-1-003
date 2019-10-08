c632c679eb53c08382fe67f3bcc766925a3c460d
gabriela <gykimura10@gmail.com>
2019-10-08 12:16:44 -0300

    Adiciona funcao leitura arquivo

---
+++ leitura_arquivo.py
@@ -0,0 +1,17 @@
+
+
+def leitura_arquivo (arquivo_nome):
+	arquivo = open(arquivo_nome,"r")
+	
+	with open(arquivo_nome) as arq:
+		numero_sentencas = int(arq.readline())
+		sentencas = arq.read().splitlines()
+
+	return numero_sentencas, sentencas
+
+
+
+
+
+
+
